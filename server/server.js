14597349a0c20dc6a28a3cdef54362efad86c1cf
LoN <theseashoreph@gmail.com>
2025-04-02 12:04:03 +0800

    test

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ const db = mysql.createConnection({
 db.connect()
 
 app.use(cors({
-  origin: ['http://localhost:8000', 'http://localhost:8080', 'https://mern-repo.github.io', 'https://mern-repo-github-io.onrender.com'],
+  // origin: ['http://localhost:8000', 'http://localhost:8080', 'https://mern-repo.github.io', 'https://mern-repo-github-io.onrender.com'],
+  origin: ['https://mern-repo.github.io', 'https://mern-repo-github-io.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
